0b797f89385794d8e005904d4c226f6dfd9d1311
andresalonso <axalonso2@gmail.com>
2025-11-17 16:21:48 -0500

    auto and teleop updates, team decided to not do vision

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "AutoOp")
+public class AutoOp extends LinearOpMode {
+
+    DcMotor fl, fr, bl, br;
+
+    public static final double DRIVE_SPEED = 0.6;
+    public static final double TURN_SPEED = 0.6;
+
+    @Override
+    public void runOpMode() {
+
+        fl = hardwareMap.get(DcMotor.class, "frontLeft");
+        fr = hardwareMap.get(DcMotor.class, "frontRight");
+        bl = hardwareMap.get(DcMotor.class, "backLeft");
+        br = hardwareMap.get(DcMotor.class, "backRight");
+
+        fr.setDirection(DcMotor.Direction.REVERSE);
+        br.setDirection(DcMotor.Direction.REVERSE);
+
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        if (!opModeIsActive()) return;
+
+        drive(0, 1, 1000, DRIVE_SPEED);
+        drive(1, 0, 600, DRIVE_SPEED);
+        rotate(90, TURN_SPEED);
+        drive(0, -1, 900, DRIVE_SPEED);
+    }
+
+    public void drive(double x, double y, int ticks, double speed) {
+        int flTarget = fl.getCurrentPosition() + (int)((y + x) * ticks);
+        int frTarget = fr.getCurrentPosition() + (int)((y - x) * ticks);
+        int blTarget = bl.getCurrentPosition() + (int)((y - x) * ticks);
+        int brTarget = br.getCurrentPosition() + (int)((y + x) * ticks);
+
+        fl.setTargetPosition(flTarget);
+        fr.setTargetPosition(frTarget);
+        bl.setTargetPosition(blTarget);
+        br.setTargetPosition(brTarget);
+
+        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fl.setPower(speed);
+        fr.setPower(speed);
+        bl.setPower(speed);
+        br.setPower(speed);
+
+        while (opModeIsActive() && fl.isBusy() && fr.isBusy() && bl.isBusy() && br.isBusy()) {}
+
+        stopAll();
+    }
+
+    public void rotate(double degrees, double speed) {
+        int ticksPerDegree = 10;
+        int ticks = (int)(degrees * ticksPerDegree);
+
+        fl.setTargetPosition(fl.getCurrentPosition() + ticks);
+        bl.setTargetPosition(bl.getCurrentPosition() + ticks);
+        fr.setTargetPosition(fr.getCurrentPosition() - ticks);
+        br.setTargetPosition(br.getCurrentPosition() - ticks);
+
+        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fl.setPower(speed);
+        fr.setPower(speed);
+        bl.setPower(speed);
+        br.setPower(speed);
+
+        while (opModeIsActive() && fl.isBusy() && fr.isBusy() && bl.isBusy() && br.isBusy()) {}
+
+        stopAll();
+    }
+
+    public void stopAll() {
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleOp.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "DriveTeleOp")
+public class DriveTeleOp extends LinearOpMode {
+
+    DcMotor fl, fr, bl, br;
+    DcMotor intake, outtake;
+
+    public static final double INTAKE_SPEED = 0.8;
+    public static final double OUTTAKE_SPEED = 0.8;
+    public static final double SLOW_MODE_SPEED = 0.4;
+
+    boolean slowMode = false;
+    boolean bPressedLast = false;
+
+    private double expo(double i) {
+        return i * Math.abs(i) * Math.abs(i);
+    }
+
+    @Override
+    public void runOpMode() {
+
+        fl = hardwareMap.get(DcMotor.class, "frontLeft");
+        fr = hardwareMap.get(DcMotor.class, "frontRight");
+        bl = hardwareMap.get(DcMotor.class, "backLeft");
+        br = hardwareMap.get(DcMotor.class, "backRight");
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        outtake = hardwareMap.get(DcMotor.class, "outtake");
+
+        fr.setDirection(DcMotor.Direction.REVERSE);
+        br.setDirection(DcMotor.Direction.REVERSE);
+
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.b && !bPressedLast) slowMode = !slowMode;
+            bPressedLast = gamepad1.b;
+
+            double y = expo(-gamepad1.left_stick_y);
+            double x = expo(gamepad1.left_stick_x);
+            double r = expo(gamepad1.right_stick_x);
+
+            double flPow = y + x + r;
+            double frPow = y - x - r;
+            double blPow = y - x + r;
+            double brPow = y + x - r;
+
+            double max = Math.max(1.0, Math.max(
+                    Math.abs(flPow),
+                    Math.max(Math.abs(frPow),
+                            Math.max(Math.abs(blPow), Math.abs(brPow)))
+            ));
+
+            double scale = slowMode ? SLOW_MODE_SPEED : 1.0;
+
+            fl.setPower((flPow / max) * scale);
+            fr.setPower((frPow / max) * scale);
+            bl.setPower((blPow / max) * scale);
+            br.setPower((brPow / max) * scale);
+
+            if (gamepad2.right_trigger > 0.1) intake.setPower(INTAKE_SPEED);
+            else if (gamepad2.left_trigger > 0.1) intake.setPower(-INTAKE_SPEED);
+            else intake.setPower(0);
+
+            if (gamepad2.a) outtake.setPower(OUTTAKE_SPEED);
+            else if (gamepad2.b) outtake.setPower(-OUTTAKE_SPEED);
+            else outtake.setPower(0);
+
+            telemetry.addData("slowMode", slowMode);
+            telemetry.update();
+        }
+    }
+}
